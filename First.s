07bdb1d7df7c748201d32758fef85c86479eb280
AmirMzl06 <134225485+AmirMzl06@users.noreply.github.com>
2025-12-15 10:36:11 +0330

    Update First.s

--- First.s
+++ First.s
@@ -6,7 +6,20 @@ extern scanf
 section .data
 ab_scanf_format: db "%lld %lld",0 ;a b
 printf_format: db "%lld %lld %lld", 10, 0 ;gcd x y
-
+A dq 0
+B dq 0
+isChanged dq 0
+Nloop dq 0
+Nloopp dq 0
+x dq 1
+y dq 0
+x0 dq 1
+y0 dq 0
+x1 dq 0
+y1 dq 1
+counter dq 1
+a dq 0
+b dq 0
 section .text
 main:
     sub rsp, 24
@@ -17,92 +30,88 @@ main:
     xor rax, rax
     call scanf
 
-    mov r8, [rsp]         ; r8 = a
-    mov r9, [rsp+8]       ; r9 = b
-    mov r10,0 ;tedad tekrar shodan loop
-    mov r11,1 ;x
-    mov r12,0 ;y
-    mov r13,1 ;x0
-    mov r14,0 ;y0
-    mov r15,0 ;x1
-    mov rdi,1 ;y1
+    mov A, [rsp]         ; r8 = a A kochikas
+    mov B, [rsp+8]       ; r9 = b B bozorgas
 
-    cmp r8, r9
+    cmp A, B
     jg Change
-    cmp r8, 0
+    cmp A, 0
     je End
     jmp FindGCD
 
 Change:
-    mov rcx, r8
-    mov r8, r9
-    mov r9, rcx
-
+    mov rcx, A
+    mov A, B
+    mov B, rcx
+    mov isChanged,1
 FindGCD:
-    sub rsp,16 ; baraye zakhireye 
     xor rdx, rdx
-    mov [rsp+8],r9
-    mov [rsp],r8
     
-    mov rax, r9
-    div r8
+    mov rax, B
+    div A
     
-    mov r9, r8
-    mov r8, rdx
+    push A
+    push B
     
-    cmp r8, 0
-    jne FindXY
+    mov B, A
+    mov A, rdx
+
     
-    inc r10
-    mov rbp,r10
-    jmp FindGCD
-    
+    add Nloop,1
+
+    cmp B,0
+    jne FindGCD
+
 FindAB:
-    mov r13,r11 ;save x ghabli
+    cmp Nloop,0
+    je Chn
+    pop b
+    pop a
+    
+    mov rax,a
+    cqo
+    idiv b
 
-    shl r14,4  ;r14 = r14*16
-    add r14,24
-    ;a
-    mov r8 ,dword [rsp + r14 -8]
-    ;b
-    mov r9 , dword[rsp+r14-16]
-    
-    mov rax,r8
-    idiv r9
-    ;rax javab taghsim
-    
-    ;X's
-    mov rsi,rax
-    imul rsi,r15
-    imul rsi,-1
-    add rsi,r13
-    mov r11,rsi
-    mov r13,r15
-    mov r15,r11
-    
-    ;Y's
-    imul rax,rdi
-    imul rax,-1
-    add rax,r14
-    mov r12,rax
-    mov r14,rdi
-    mov rdi,rax
-    
-    inc r10
-    
+    ; update x
+    mov r9,x1
+    imul r9,rax
+    mov r10,x0
+    sub r10,r9
+    mov x,r10
+    mov r9,x1
+    mov x0,r9
+    mov x1,x
 
+    ; update y
+    mov r9,y1
+    imul r9,rax
+    mov r10,y0
+    sub r10,r9
+    mov y,r10
+    mov r9,y1
+    mov y0,r9
+    mov y1,y
+
+    dec Nloop
+    jmp FindAB
+
+Chn:
+    cmp isChanged,1
+    je Chng
+    jump End
+
+Chng:
+    mov x0,x
+    mov x,y
+    mov y,x0
 End:
     mov rdi, printf_format
-    mov rsi, r9
-    mov rdx,r11
-    mov rcx,r12
+    mov rsi, A    
+    mov rdx, x
+    mov rcx, y
     xor rax, rax
     call printf
 
-    mov rax, rbp
-    inc rax
-    shl rax,4
-    add rax, 24
-    add rsp, rax
+    add rsp, 24
     mov eax, 0
     ret
