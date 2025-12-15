11497a9bcbc7e0e1902fdea010f957d37529e8f7
AmirMzl06 <134225485+AmirMzl06@users.noreply.github.com>
2025-12-15 10:46:44 +0330

    Update First.s

--- First.s
+++ First.s
@@ -4,114 +4,138 @@ extern printf
 extern scanf
 
 section .data
-ab_scanf_format: db "%lld %lld",0 ;a b
-printf_format: db "%lld %lld %lld", 10, 0 ;gcd x y
+ab_scanf_format: db "%lld %lld",0
+printf_format:   db "%lld %lld %lld",10,0   ; gcd x y
+
 A dq 0
 B dq 0
 isChanged dq 0
 Nloop dq 0
-Nloopp dq 0
-x dq 1
-y dq 0
+
+x  dq 1
+y  dq 0
 x0 dq 1
 y0 dq 0
 x1 dq 0
 y1 dq 1
-counter dq 1
+
 a dq 0
 b dq 0
+
 section .text
 main:
-    sub rsp, 24
+    sub rsp, 32
 
     mov rdi, ab_scanf_format
-    mov rsi, rsp          ; a (farz a > b)
-    lea rdx, dword [rsp+8]      ; b
+    lea rsi, [rsp]
+    lea rdx, [rsp+8]
     xor rax, rax
     call scanf
 
-    mov A, [rsp]         ; r8 = a A kochikas
-    mov B, [rsp+8]       ; r9 = b B bozorgas
+    mov rax, [rsp]
+    mov [A], rax
+    mov rax, [rsp+8]
+    mov [B], rax
 
-    cmp A, B
-    jg Change
-    cmp A, 0
+    mov qword [Nloop], 0
+    mov qword [isChanged], 0
+
+    mov rax, [A]
+    cmp rax, 0
     je End
-    jmp FindGCD
+
+    mov rax, [A]
+    cmp rax, [B]
+    jle FindGCD
 
 Change:
-    mov rcx, A
-    mov A, B
-    mov B, rcx
-    mov isChanged,1
+    mov rax, [A]
+    mov rbx, [B]
+    mov [A], rbx
+    mov [B], rax
+    mov qword [isChanged], 1
+
 FindGCD:
-    xor rdx, rdx
+    mov rax, [B]
+    cqo
+    idiv qword [A]
     
-    mov rax, B
-    div A
-    
-    push A
-    push B
-    
-    mov B, A
-    mov A, rdx
+    mov rbx, [A]
+    push rbx
+    mov rbx, [B]
+    push rbx
 
-    
-    add Nloop,1
+    mov rbx, [A]
+    mov [B], rbx
+    mov [A], rdx
 
-    cmp B,0
+    mov rax, [Nloop]
+    inc rax
+    mov [Nloop], rax
+
+    cmp rdx, 0
     jne FindGCD
 
 FindAB:
-    cmp Nloop,0
+    mov rax, [Nloop]
+    cmp rax, 0
     je Chn
-    pop b
-    pop a
-    
-    mov rax,a
+
+    pop rax
+    mov [b], rax
+    pop rax
+    mov [a], rax
+
+    mov rax, [a]
     cqo
-    idiv b
+    idiv qword [b]
 
-    ; update x
-    mov r9,x1
-    imul r9,rax
-    mov r10,x0
-    sub r10,r9
-    mov x,r10
-    mov r9,x1
-    mov x0,r9
-    mov x1,x
+    ; x update
+    mov r9, [x1]
+    imul r9, rax
+    mov r10, [x0]
+    sub r10, r9
+    mov [x], r10
 
-    ; update y
-    mov r9,y1
-    imul r9,rax
-    mov r10,y0
-    sub r10,r9
-    mov y,r10
-    mov r9,y1
-    mov y0,r9
-    mov y1,y
+    mov r9, [x1]
+    mov [x0], r9
+    mov r9, [x]
+    mov [x1], r9
 
-    dec Nloop
+    ; y update
+    mov r9, [y1]
+    imul r9, rax
+    mov r10, [y0]
+    sub r10, r9
+    mov [y], r10
+
+    mov r9, [y1]
+    mov [y0], r9
+    mov r9, [y]
+    mov [y1], r9
+
+    mov rax, [Nloop]
+    dec rax
+    mov [Nloop], rax
     jmp FindAB
 
 Chn:
-    cmp isChanged,1
-    je Chng
-    jump End
+    cmp qword [isChanged], 1
+    jne End
 
-Chng:
-    mov x0,x
-    mov x,y
-    mov y,x0
+    mov rax, [x]
+    mov rbx, [y]
+    mov [x], rbx
+    mov [y], rax
+
 End:
     mov rdi, printf_format
-    mov rsi, A    
-    mov rdx, x
-    mov rcx, y
+    mov rsi, [B]    ; gcd
+    mov rdx, [x]
+    mov rcx, [y]
     xor rax, rax
     call printf
 
-    add rsp, 24
-    mov eax, 0
+    add rsp, 32
+    xor eax, eax
     ret
