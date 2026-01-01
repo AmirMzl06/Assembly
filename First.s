0ee265122095653d81f4ae7fd1551b2a0566ab8d
AmirMzl06 <134225485+AmirMzl06@users.noreply.github.com>
2026-01-01 18:02:07 +0330

    Update First.s

--- First.s
+++ First.s
@@ -1,20 +1,18 @@
-global main
+global asm_main
 extern printf
 extern scanf
 
 section .data
 scanf_format db "%lld %lld %lld",0
-printf_format db "%lld",0,10
-String db "%s",0,10
+printf_format db "%lld",10,0
+String db "%s",10,0
 
 False db "FALSE",0
 True db "TRUE",0
 
 output: dq printf_format,printf_format,printf_format,printf_format,printf_format,String,String,String,String,String
-TorF: dq False,True
 
 result dq 0
-
 a1 dq 0
 a2 dq 0
 id dq 0
@@ -23,20 +21,24 @@ section .bss
 idArr resq 10
 
 section .text
-main:
+asm_main:
     sub rsp, 24
 
     mov rdi, scanf_format
-    lea rsi,[rsp] ;id 
-    lea rdx ,[rsp+8] ;a1
+    lea rsi,[rsp]    ;id 
+    lea rdx ,[rsp+8]  ;a1
     lea rcx,[rsp+16] ;a2
+    xor al, al
     call scanf
 
-    mov [a1],[rsp+8]
-    mov [a2],[rsp+16]
-    mov [id],[rsp]
+    mov rax, [rsp]
+    mov [id], rax
+    mov rax, [rsp+8]
+    mov [a1], rax
+    mov rax, [rsp+16]
+    mov [a2], rax
 
-    mov r9,[rsp]
+    mov r9, [id]
     mov qword [idArr + r9*8], 1
 
     ;0
@@ -65,46 +67,56 @@ main:
 
     ;4
     mov rax,[a1]
-    mov rbx,[a2]
-    mov rdx,0
-    imul rax, rbx
+    imul qword[a2]
     mov rax, rdx
     imul rax, qword[idArr+32]
     add qword[result],rax
 
     ;5
     mov rax,[a1]
-    mov rbx,[a2]
-    cmp rax, rbx
+    cmp rax, [a2]
     setg al
     movzx rax, al
+    mov rbx, True
+    sub rbx, False
+    imul rax, rbx
+    add rax, False
     imul rax, qword[idArr+40]
     add qword[result],rax
 
     ;6
     mov rax,[a1]
-    mov rbx,[a2]
-    cmp rax, rbx
+    cmp rax, [a2]
     setge al
     movzx rax, al
+    mov rbx, True
+    sub rbx, False
+    imul rax, rbx
+    add rax, False
     imul rax, qword[idArr+48]
     add qword[result],rax
 
     ;7
     mov rax,[a1]
-    mov rbx,[a2]
-    cmp rax, rbx
+    cmp rax, [a2]
     setb al
     movzx rax, al
+    mov rbx, True
+    sub rbx, False
+    imul rax, rbx
+    add rax, False
     imul rax, qword[idArr+56]
     add qword[result],rax
 
     ;8
     mov rax,[a1]
-    mov rbx,[a2]
-    cmp rax, rbx
+    cmp rax, [a2]
     setbe al
     movzx rax, al
+    mov rbx, True
+    sub rbx, False
+    imul rax, rbx
+    add rax, False
     imul rax, qword[idArr+64]
     add qword[result],rax
 
@@ -113,12 +125,17 @@ main:
     add rax,[a2]
     seto al
     movzx rax, al
+    mov rbx, True
+    sub rbx, False
+    imul rax, rbx
+    add rax, False
     imul rax, qword[idArr+72]
     add qword[result],rax
 
     mov rcx,[id]
-    mov rdi,[output+ rcx*8]
-    mov rsi,result
+    mov rdi,[output + rcx*8]
+    mov rsi,[result]
+    mov al, 0
     call printf
 
     add rsp, 24
