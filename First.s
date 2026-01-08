bb1dd0762d929c996a5572acee7cbddac48097b0
AmirMzl06 <134225485+AmirMzl06@users.noreply.github.com>
2026-01-08 12:25:34 +0330

    Update First.s

--- First.s
+++ First.s
@@ -25,7 +25,10 @@ section .data
     output2     db "%.3f", 10, 0
     newline     db 10, 0
     
+    ; ماسک برای قدر مطلق (همه بیت‌ها 1 بجز بیت علامت)
     abs_mask    dq 0x7FFFFFFFFFFFFFFF
+    ; ماسک جدید برای قرینه کردن (فقط بیت علامت 1 است)
+    neg_mask    dq 0x8000000000000000
 
 section .bss
     n           resq 1
@@ -36,8 +39,10 @@ section .bss
 
 section .text
 main:
-    sub rsp, 8
+    push rbx        ; ذخیره رجیستر RBX چون از آن استفاده خواهیم کرد (Callee-saved)
+    sub rsp, 8      ; تراز کردن پشته (Stack Alignment)
     
+    ; دریافت تعداد اعداد (n)
     mov rdi, n_input
     mov rsi, n
     xor eax, eax
@@ -47,14 +52,14 @@ main:
     mov rdi, array
 .get_number_loop:
     push rcx
-    push rdi
+    push rdi        ; آدرس فعلی آرایه را نگه می‌داریم
     mov rsi, rdi
     mov rdi, input
     xor eax, eax
     call scanf
-    pop rdi
+    pop rdi         ; آدرس آرایه را بازیابی می‌کنیم
     pop rcx
-    add rdi, 8
+    add rdi, 8      ; رفتن به خانه بعدی (double = 8 bytes)
     loop .get_number_loop
 
 .get_command:
@@ -65,11 +70,13 @@ main:
     
     mov rdi, cmd
     
+    ; بررسی دستور exit
     mov rsi, exit_str
     call strcmp
     test rax, rax
     jz .do_exit
     
+    ; بررسی دستورات تک آرگومانی
     mov rsi, neg_str
     call strcmp
     test rax, rax
@@ -91,6 +98,7 @@ main:
     jz .read_one_and_dispatch
 
 .read_two_and_dispatch:
+    ; خواندن دو ایندکس برای دستورات بازه‌ای
     mov rdi, n_input
     mov rsi, idx1
     xor eax, eax
@@ -103,6 +111,7 @@ main:
     jmp .dispatch
 
 .read_one_and_dispatch:
+    ; خواندن یک ایندکس برای دستورات تکی
     mov rdi, n_input
     mov rsi, idx1
     xor eax, eax
@@ -111,6 +120,7 @@ main:
 .dispatch:
     mov rdi, cmd
     
+    ; مقایسه مجدد برای پرش به لیبل مناسب
     mov rsi, neg_str
     call strcmp
     test rax, rax
@@ -180,14 +190,15 @@ main:
 
 .do_exit:
     add rsp, 8
+    pop rbx         ; بازیابی RBX
     xor eax, eax
     ret
 
 .do_neg:
     mov rdx, [idx1]
     movsd xmm0, [array + rdx*8]
-    movq xmm1, [abs_mask]
-    xorpd xmm0, xmm1
+    movq xmm1, [neg_mask]       ; اصلاح: استفاده از ماسک بیت علامت
+    xorpd xmm0, xmm1            ; XOR با 0x800... بیت علامت را عوض می‌کند
     movsd [array + rdx*8], xmm0
     jmp .get_command
 
@@ -207,7 +218,7 @@ main:
 .do_round:
     mov rdx, [idx1]
     movsd xmm0, [array + rdx*8]
-    roundsd xmm0, xmm0, 0
+    roundsd xmm0, xmm0, 0       ; 0 = Round to nearest integer
     movsd [array + rdx*8], xmm0
     jmp .get_command
 
@@ -216,18 +227,18 @@ main:
 .outerA:
     mov r12, [idx2]
     cmp r11, r12
-    jge .get_command
-    mov r14, r11
+    jge .get_command        ; Selection sort تا یکی مانده به آخر کافیست
+    mov r14, r11            ; r14 = ایندکس مینیمم
     mov r13, r11
 .innerA:
     inc r13
     cmp r13, r12
-    jg .swapA
-    movsd xmm0, [array + r13*8]
-    movsd xmm1, [array + r14*8]
+    jg .swapA               ; اصلاح: jg برای بررسی تا آخرین عنصر (شامل idx2)
+    movsd xmm0, [array + r13*8] ; عدد جاری
+    movsd xmm1, [array + r14*8] ; عدد مینیمم فعلی
     ucomisd xmm1, xmm0
-    jbe .innerA
-    mov r14, r13
+    jbe .innerA             ; اگر min <= curr ادامه بده
+    mov r14, r13            ; وگرنه ایندکس مینیمم جدید را نگه دار
     jmp .innerA
 .swapA:
     movsd xmm0, [array + r11*8]
@@ -266,11 +277,11 @@ main:
 .do_maxc:
     mov r11, [idx1]
     mov r12, [idx2]
-    movsd xmm0, [array + r11*8]
+    movsd xmm0, [array + r11*8] ; xmm0 نگهدارنده ماکسیمم
 .findMaxC:
     inc r11
     cmp r11, r12
-    jge .fillMaxC
+    jg .fillMaxC            ; اصلاح: jg بجای jge تا عنصر آخر هم چک شود
     movsd xmm1, [array + r11*8]
     ucomisd xmm0, xmm1
     jae .findMaxC
@@ -281,7 +292,7 @@ main:
 .fillLoopC:
     mov r12, [idx2]
     cmp r11, r12
-    jg .get_command
+    jg .get_command         ; اصلاح: jg
     movsd [array + r11*8], xmm0
     inc r11
     jmp .fillLoopC
@@ -289,20 +300,20 @@ main:
 .do_lowa:
     mov r11, [idx1]
     mov r12, [idx2]
-    movsd xmm0, [array + r11*8]
+    movsd xmm0, [array + r11*8] ; xmm0 = مقدار نهایی
     movsd xmm1, xmm0
-    andpd xmm1, [abs_mask]
+    andpd xmm1, [abs_mask]      ; xmm1 = قدرمطلق مینیمم فعلی
 .findLowa:
     inc r11
     cmp r11, r12
-    jg .fillLowa
-    movsd xmm2, [array + r11*8]
+    jg .fillLowa            ; اصلاح: jg
+    movsd xmm2, [array + r11*8] ; مقدار جدید
     movsd xmm3, xmm2
-    andpd xmm3, [abs_mask]
+    andpd xmm3, [abs_mask]      ; قدرمطلق جدید
     ucomisd xmm1, xmm3
-    jbe .findLowa
-    movsd xmm1, xmm3
-    movsd xmm0, xmm2
+    jbe .findLowa               ; اگر min_abs <= new_abs ادامه بده
+    movsd xmm1, xmm3            ; آپدیت قدر مطلق
+    movsd xmm0, xmm2            ; آپدیت مقدار واقعی
     jmp .findLowa
 .fillLowa:
     mov r11, [idx1]
@@ -315,17 +326,18 @@ main:
     jmp .fillLoopLowa
 
 .do_print:
-    mov r11, [idx1]
+    ; اصلاح مهم: استفاده از RBX بجای R11
+    ; printf رجیستر R11 را تغییر می‌دهد، پس نمی‌توان از آن به عنوان شمارنده استفاده کرد
+    mov rbx, [idx1]
     mov r12, [idx2]
 .print_loop:
-    inc r12
-    cmp r11, r12
-    jge .print_newline
-    movsd xmm0, [array + r11*8]
+    cmp rbx, r12
+    jg .print_newline       ; اصلاح: jg برای چاپ آخرین عنصر
+    movsd xmm0, [array + rbx*8]
     mov rdi, output
     mov eax, 1
     call printf
-    inc r11
+    inc rbx
     jmp .print_loop
 .print_newline:
     mov rdi, newline
@@ -340,7 +352,7 @@ main:
 .max_loop:
     inc r11
     cmp r11, r12
-    jg .print_val
+    jg .print_val           ; اصلاح: jg
     movsd xmm1, [array + r11*8]
     ucomisd xmm0, xmm1
     jae .max_loop
@@ -354,7 +366,7 @@ main:
 .min_loop:
     inc r11
     cmp r11, r12
-    jg .print_val
+    jg .print_val           ; اصلاح: jg
     movsd xmm1, [array + r11*8]
     ucomisd xmm0, xmm1
     jbe .min_loop
@@ -370,7 +382,7 @@ main:
 .low_loop:
     inc r11
     cmp r11, r12
-    jg .print_val
+    jg .print_val           ; اصلاح: jg
     movsd xmm2, [array + r11*8]
     movsd xmm3, xmm2
     andpd xmm3, [abs_mask]
@@ -389,7 +401,7 @@ main:
 .high_loop:
     inc r11
     cmp r11, r12
-    jg .print_val
+    jg .print_val           ; اصلاح: jg
     movsd xmm2, [array + r11*8]
     movsd xmm3, xmm2
     andpd xmm3, [abs_mask]
