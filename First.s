913c85de7ad0166d2ae1ac697c4e597dd80e4d04
AmirMzl06 <134225485+AmirMzl06@users.noreply.github.com>
2026-06-15 13:06:04 -0400

    Update First.s

--- First.s
+++ First.s
@@ -1,144 +1,144 @@
 --- Training Baseline Model ---
-Epoch 001/150 | Loss: 0.8226 | R2: 0.2905
-Epoch 005/150 | Loss: 0.1287 | R2: 0.7045
-Epoch 010/150 | Loss: 0.0498 | R2: 0.7151
-Epoch 015/150 | Loss: 0.0263 | R2: 0.6966
-Epoch 020/150 | Loss: 0.0173 | R2: 0.6643
-Epoch 025/150 | Loss: 0.0129 | R2: 0.6494
-Epoch 030/150 | Loss: 0.0108 | R2: 0.6319
-Epoch 035/150 | Loss: 0.0081 | R2: 0.6352
-Epoch 040/150 | Loss: 0.0062 | R2: 0.6176
-Epoch 045/150 | Loss: 0.0053 | R2: 0.6172
-Epoch 050/150 | Loss: 0.0088 | R2: 0.6079
-Epoch 055/150 | Loss: 0.0047 | R2: 0.6184
-Epoch 060/150 | Loss: 0.0053 | R2: 0.6139
-Epoch 065/150 | Loss: 0.0055 | R2: 0.6126
-Epoch 070/150 | Loss: 0.0045 | R2: 0.6024
-Epoch 075/150 | Loss: 0.0045 | R2: 0.6083
-Epoch 080/150 | Loss: 0.0033 | R2: 0.5934
-Epoch 085/150 | Loss: 0.0022 | R2: 0.5932
-Epoch 090/150 | Loss: 0.0020 | R2: 0.5853
-Epoch 095/150 | Loss: 0.0021 | R2: 0.5808
-Epoch 100/150 | Loss: 0.0029 | R2: 0.5937
-Epoch 105/150 | Loss: 0.0017 | R2: 0.5823
-Epoch 110/150 | Loss: 0.0027 | R2: 0.5758
-Epoch 115/150 | Loss: 0.0039 | R2: 0.5644
-Epoch 120/150 | Loss: 0.0028 | R2: 0.5586
-Epoch 125/150 | Loss: 0.0014 | R2: 0.5656
-Epoch 130/150 | Loss: 0.0019 | R2: 0.5654
-Epoch 135/150 | Loss: 0.0014 | R2: 0.5584
-Epoch 140/150 | Loss: 0.0017 | R2: 0.5619
-Epoch 145/150 | Loss: 0.0034 | R2: 0.5573
-Epoch 150/150 | Loss: 0.0022 | R2: 0.5530
-Baseline Time: 10.89s | Final R2: 0.5530
+Epoch 001/150 | Loss: 0.7575 | R2: 0.2969
+Epoch 005/150 | Loss: 0.1266 | R2: 0.6833
+Epoch 010/150 | Loss: 0.0767 | R2: 0.6783
+Epoch 015/150 | Loss: 0.0586 | R2: 0.6560
+Epoch 020/150 | Loss: 0.0452 | R2: 0.6557
+Epoch 025/150 | Loss: 0.0375 | R2: 0.6222
+Epoch 030/150 | Loss: 0.0302 | R2: 0.6189
+Epoch 035/150 | Loss: 0.0275 | R2: 0.5879
+Epoch 040/150 | Loss: 0.0201 | R2: 0.5886
+Epoch 045/150 | Loss: 0.0152 | R2: 0.5523
+Epoch 050/150 | Loss: 0.0140 | R2: 0.5806
+Epoch 055/150 | Loss: 0.0124 | R2: 0.5695
+Epoch 060/150 | Loss: 0.0143 | R2: 0.5683
+Epoch 065/150 | Loss: 0.0102 | R2: 0.5933
+Epoch 070/150 | Loss: 0.0105 | R2: 0.5988
+Epoch 075/150 | Loss: 0.0110 | R2: 0.5814
+Epoch 080/150 | Loss: 0.0117 | R2: 0.5399
+Epoch 085/150 | Loss: 0.0072 | R2: 0.5759
+Epoch 090/150 | Loss: 0.0066 | R2: 0.5656
+Epoch 095/150 | Loss: 0.0073 | R2: 0.5783
+Epoch 100/150 | Loss: 0.0054 | R2: 0.5515
+Epoch 105/150 | Loss: 0.0044 | R2: 0.5607
+Epoch 110/150 | Loss: 0.0042 | R2: 0.5699
+Epoch 115/150 | Loss: 0.0039 | R2: 0.5629
+Epoch 120/150 | Loss: 0.0081 | R2: 0.5793
+Epoch 125/150 | Loss: 0.0067 | R2: 0.5696
+Epoch 130/150 | Loss: 0.0056 | R2: 0.5901
+Epoch 135/150 | Loss: 0.0049 | R2: 0.5673
+Epoch 140/150 | Loss: 0.0069 | R2: 0.5655
+Epoch 145/150 | Loss: 0.0035 | R2: 0.5974
+Epoch 150/150 | Loss: 0.0032 | R2: 0.5945
+Baseline Time: 10.56s | Final R2: 0.5945
 --- Training TANR64 ---
-Epoch 001/150 | Loss: 0.9797 | R2: 0.2991
-Epoch 005/150 | Loss: 0.3342 | R2: 0.6647
-Epoch 010/150 | Loss: 0.1752 | R2: 0.6772
-Epoch 015/150 | Loss: 0.1168 | R2: 0.6659
-Epoch 020/150 | Loss: 0.0915 | R2: 0.6524
-Epoch 025/150 | Loss: 0.0790 | R2: 0.6327
-Epoch 030/150 | Loss: 0.0651 | R2: 0.6481
-Epoch 035/150 | Loss: 0.0589 | R2: 0.6591
-Epoch 040/150 | Loss: 0.0528 | R2: 0.6354
-Epoch 045/150 | Loss: 0.0476 | R2: 0.6311
-Epoch 050/150 | Loss: 0.0448 | R2: 0.6248
-Epoch 055/150 | Loss: 0.0420 | R2: 0.6312
-Epoch 060/150 | Loss: 0.0381 | R2: 0.6326
-Epoch 065/150 | Loss: 0.0354 | R2: 0.6143
-Epoch 070/150 | Loss: 0.0342 | R2: 0.6201
-Epoch 075/150 | Loss: 0.0315 | R2: 0.6046
-Epoch 080/150 | Loss: 0.0299 | R2: 0.5915
-Epoch 085/150 | Loss: 0.0283 | R2: 0.6004
-Epoch 090/150 | Loss: 0.0260 | R2: 0.6162
-Epoch 095/150 | Loss: 0.0248 | R2: 0.5936
-Epoch 100/150 | Loss: 0.0246 | R2: 0.6071
-Epoch 105/150 | Loss: 0.0231 | R2: 0.6089
-Epoch 110/150 | Loss: 0.0218 | R2: 0.6001
-Epoch 115/150 | Loss: 0.0216 | R2: 0.6129
-Epoch 120/150 | Loss: 0.0199 | R2: 0.6202
-Epoch 125/150 | Loss: 0.0190 | R2: 0.6034
-Epoch 130/150 | Loss: 0.0180 | R2: 0.6149
-Epoch 135/150 | Loss: 0.0170 | R2: 0.5986
-Epoch 140/150 | Loss: 0.0180 | R2: 0.6002
-Epoch 145/150 | Loss: 0.0173 | R2: 0.5944
-Epoch 150/150 | Loss: 0.0151 | R2: 0.6110
-TANR64 Time: 21.16s | Final R2: 0.6110
-Baseline Latent Space PCA explained variance: 0.8250
-TANR64 Latent Space PCA explained variance: 0.5611
+Epoch 001/150 | Loss: 1.0684 | R2: 0.3211
+Epoch 005/150 | Loss: 0.4742 | R2: 0.7122
+Epoch 010/150 | Loss: 0.3273 | R2: 0.7025
+Epoch 015/150 | Loss: 0.2449 | R2: 0.6897
+Epoch 020/150 | Loss: 0.1953 | R2: 0.6496
+Epoch 025/150 | Loss: 0.1652 | R2: 0.6279
+Epoch 030/150 | Loss: 0.1505 | R2: 0.6311
+Epoch 035/150 | Loss: 0.1389 | R2: 0.6252
+Epoch 040/150 | Loss: 0.1281 | R2: 0.5748
+Epoch 045/150 | Loss: 0.1181 | R2: 0.6350
+Epoch 050/150 | Loss: 0.1160 | R2: 0.6133
+Epoch 055/150 | Loss: 0.1064 | R2: 0.6163
+Epoch 060/150 | Loss: 0.1013 | R2: 0.5932
+Epoch 065/150 | Loss: 0.0967 | R2: 0.5667
+Epoch 070/150 | Loss: 0.0953 | R2: 0.5529
+Epoch 075/150 | Loss: 0.0882 | R2: 0.5722
+Epoch 080/150 | Loss: 0.0865 | R2: 0.6005
+Epoch 085/150 | Loss: 0.0807 | R2: 0.5694
+Epoch 090/150 | Loss: 0.0833 | R2: 0.6279
+Epoch 095/150 | Loss: 0.0770 | R2: 0.6217
+Epoch 100/150 | Loss: 0.0729 | R2: 0.5808
+Epoch 105/150 | Loss: 0.0743 | R2: 0.5884
+Epoch 110/150 | Loss: 0.0761 | R2: 0.6093
+Epoch 115/150 | Loss: 0.0723 | R2: 0.6139
+Epoch 120/150 | Loss: 0.0698 | R2: 0.5604
+Epoch 125/150 | Loss: 0.0700 | R2: 0.6105
+Epoch 130/150 | Loss: 0.0677 | R2: 0.5605
+Epoch 135/150 | Loss: 0.0666 | R2: 0.5448
+Epoch 140/150 | Loss: 0.0688 | R2: 0.5754
+Epoch 145/150 | Loss: 0.0644 | R2: 0.5363
+Epoch 150/150 | Loss: 0.0685 | R2: 0.5239
+TANR64 Time: 20.21s | Final R2: 0.5239
+Baseline Latent Space PCA explained variance: 0.7332
+TANR64 Latent Space PCA explained variance: 0.3530
 --- Training TANR8 ---
-Epoch 001/150 | Loss: 1.0675 | R2: -0.0718
-Epoch 005/150 | Loss: 0.6694 | R2: 0.1622
-Epoch 010/150 | Loss: 0.3748 | R2: 0.4002
-Epoch 015/150 | Loss: 0.2334 | R2: 0.5339
-Epoch 020/150 | Loss: 0.1738 | R2: 0.6094
-Epoch 025/150 | Loss: 0.1406 | R2: 0.6429
-Epoch 030/150 | Loss: 0.1196 | R2: 0.6624
-Epoch 035/150 | Loss: 0.1051 | R2: 0.6662
-Epoch 040/150 | Loss: 0.0945 | R2: 0.6621
-Epoch 045/150 | Loss: 0.0861 | R2: 0.6537
-Epoch 050/150 | Loss: 0.0795 | R2: 0.6406
-Epoch 055/150 | Loss: 0.0736 | R2: 0.6239
-Epoch 060/150 | Loss: 0.0686 | R2: 0.6206
-Epoch 065/150 | Loss: 0.0643 | R2: 0.6056
-Epoch 070/150 | Loss: 0.0609 | R2: 0.5936
-Epoch 075/150 | Loss: 0.0567 | R2: 0.5757
-Epoch 080/150 | Loss: 0.0527 | R2: 0.5608
-Epoch 085/150 | Loss: 0.0499 | R2: 0.5592
-Epoch 090/150 | Loss: 0.0467 | R2: 0.5613
-Epoch 095/150 | Loss: 0.0449 | R2: 0.5551
-Epoch 100/150 | Loss: 0.0423 | R2: 0.5511
-Epoch 105/150 | Loss: 0.0409 | R2: 0.5521
-Epoch 110/150 | Loss: 0.0388 | R2: 0.5513
-Epoch 115/150 | Loss: 0.0373 | R2: 0.5572
-Epoch 120/150 | Loss: 0.0358 | R2: 0.5419
-Epoch 125/150 | Loss: 0.0358 | R2: 0.5572
-Epoch 130/150 | Loss: 0.0340 | R2: 0.5426
-Epoch 135/150 | Loss: 0.0321 | R2: 0.5293
-Epoch 140/150 | Loss: 0.0303 | R2: 0.5255
-Epoch 145/150 | Loss: 0.0316 | R2: 0.5462
-Epoch 150/150 | Loss: 0.0278 | R2: 0.5205
-TANR8 Time: 14.56s | Final R2: 0.5205
-Baseline Latent Space PCA explained variance: 0.8250
-TANR8 Latent Space PCA explained variance: 0.6277
+Epoch 001/150 | Loss: 1.1286 | R2: -0.0860
+Epoch 005/150 | Loss: 0.7798 | R2: 0.1610
+Epoch 010/150 | Loss: 0.5218 | R2: 0.3449
+Epoch 015/150 | Loss: 0.3640 | R2: 0.4636
+Epoch 020/150 | Loss: 0.2706 | R2: 0.5404
+Epoch 025/150 | Loss: 0.2234 | R2: 0.5783
+Epoch 030/150 | Loss: 0.1917 | R2: 0.6067
+Epoch 035/150 | Loss: 0.1728 | R2: 0.6317
+Epoch 040/150 | Loss: 0.1590 | R2: 0.6477
+Epoch 045/150 | Loss: 0.1496 | R2: 0.6626
+Epoch 050/150 | Loss: 0.1433 | R2: 0.6815
+Epoch 055/150 | Loss: 0.1377 | R2: 0.6839
+Epoch 060/150 | Loss: 0.1324 | R2: 0.6792
+Epoch 065/150 | Loss: 0.1282 | R2: 0.6651
+Epoch 070/150 | Loss: 0.1257 | R2: 0.6580
+Epoch 075/150 | Loss: 0.1218 | R2: 0.6604
+Epoch 080/150 | Loss: 0.1192 | R2: 0.6401
+Epoch 085/150 | Loss: 0.1160 | R2: 0.6413
+Epoch 090/150 | Loss: 0.1131 | R2: 0.6366
+Epoch 095/150 | Loss: 0.1124 | R2: 0.6364
+Epoch 100/150 | Loss: 0.1087 | R2: 0.6287
+Epoch 105/150 | Loss: 0.1063 | R2: 0.6287
+Epoch 110/150 | Loss: 0.1041 | R2: 0.6288
+Epoch 115/150 | Loss: 0.1033 | R2: 0.6281
+Epoch 120/150 | Loss: 0.1018 | R2: 0.6188
+Epoch 125/150 | Loss: 0.1007 | R2: 0.6315
+Epoch 130/150 | Loss: 0.0989 | R2: 0.6240
+Epoch 135/150 | Loss: 0.0984 | R2: 0.5967
+Epoch 140/150 | Loss: 0.0956 | R2: 0.6128
+Epoch 145/150 | Loss: 0.0927 | R2: 0.5773
+Epoch 150/150 | Loss: 0.0904 | R2: 0.5690
+TANR8 Time: 14.36s | Final R2: 0.5690
+Baseline Latent Space PCA explained variance: 0.7332
+TANR8 Latent Space PCA explained variance: 0.7508
 --- Training TANR3 ---
-Epoch 001/150 | Loss: 1.0023 | R2: -0.0786
-Epoch 005/150 | Loss: 0.8575 | R2: -0.0048
-Epoch 010/150 | Loss: 0.7013 | R2: 0.1010
-Epoch 015/150 | Loss: 0.5568 | R2: 0.2082
-Epoch 020/150 | Loss: 0.4286 | R2: 0.3011
-Epoch 025/150 | Loss: 0.3363 | R2: 0.3551
-Epoch 030/150 | Loss: 0.2781 | R2: 0.4054
-Epoch 035/150 | Loss: 0.2383 | R2: 0.4420
-Epoch 040/150 | Loss: 0.2117 | R2: 0.4562
-Epoch 045/150 | Loss: 0.1934 | R2: 0.4671
-Epoch 050/150 | Loss: 0.1768 | R2: 0.4762
-Epoch 055/150 | Loss: 0.1631 | R2: 0.4825
-Epoch 060/150 | Loss: 0.1462 | R2: 0.4952
-Epoch 065/150 | Loss: 0.1342 | R2: 0.5030
-Epoch 070/150 | Loss: 0.1241 | R2: 0.5067
-Epoch 075/150 | Loss: 0.1143 | R2: 0.5059
-Epoch 080/150 | Loss: 0.1069 | R2: 0.5080
-Epoch 085/150 | Loss: 0.1009 | R2: 0.5167
-Epoch 090/150 | Loss: 0.0943 | R2: 0.5263
-Epoch 095/150 | Loss: 0.0888 | R2: 0.5335
-Epoch 100/150 | Loss: 0.0847 | R2: 0.5393
-Epoch 105/150 | Loss: 0.0814 | R2: 0.5409
-Epoch 110/150 | Loss: 0.0784 | R2: 0.5387
-Epoch 115/150 | Loss: 0.0749 | R2: 0.5373
-Epoch 120/150 | Loss: 0.0724 | R2: 0.5304
-Epoch 125/150 | Loss: 0.0703 | R2: 0.5272
-Epoch 130/150 | Loss: 0.0680 | R2: 0.5227
-Epoch 135/150 | Loss: 0.0657 | R2: 0.5213
-Epoch 140/150 | Loss: 0.0641 | R2: 0.5165
-Epoch 145/150 | Loss: 0.0621 | R2: 0.5062
-Epoch 150/150 | Loss: 0.0609 | R2: 0.4934
-TANR3 Time: 14.23s | Final R2: 0.4934
-Baseline Latent Space PCA explained variance: 0.8250
-TANR3 Latent Space PCA explained variance: 0.9051
+Epoch 001/150 | Loss: 1.4963 | R2: -0.5183
+Epoch 005/150 | Loss: 1.2660 | R2: -0.3008
+Epoch 010/150 | Loss: 1.0812 | R2: -0.1292
+Epoch 015/150 | Loss: 0.9377 | R2: -0.0010
+Epoch 020/150 | Loss: 0.7975 | R2: 0.1129
+Epoch 025/150 | Loss: 0.6634 | R2: 0.2276
+Epoch 030/150 | Loss: 0.5548 | R2: 0.3210
+Epoch 035/150 | Loss: 0.4587 | R2: 0.3924
+Epoch 040/150 | Loss: 0.3877 | R2: 0.4444
+Epoch 045/150 | Loss: 0.3353 | R2: 0.4859
+Epoch 050/150 | Loss: 0.2959 | R2: 0.5127
+Epoch 055/150 | Loss: 0.2629 | R2: 0.5400
+Epoch 060/150 | Loss: 0.2413 | R2: 0.5738
+Epoch 065/150 | Loss: 0.2251 | R2: 0.5991
+Epoch 070/150 | Loss: 0.2113 | R2: 0.6071
+Epoch 075/150 | Loss: 0.2020 | R2: 0.6232
+Epoch 080/150 | Loss: 0.1941 | R2: 0.6338
+Epoch 085/150 | Loss: 0.1874 | R2: 0.6420
+Epoch 090/150 | Loss: 0.1832 | R2: 0.6427
+Epoch 095/150 | Loss: 0.1777 | R2: 0.6417
+Epoch 100/150 | Loss: 0.1748 | R2: 0.6402
+Epoch 105/150 | Loss: 0.1715 | R2: 0.6381
+Epoch 110/150 | Loss: 0.1644 | R2: 0.6297
+Epoch 115/150 | Loss: 0.1622 | R2: 0.6280
+Epoch 120/150 | Loss: 0.1603 | R2: 0.6211
+Epoch 125/150 | Loss: 0.1591 | R2: 0.6155
+Epoch 130/150 | Loss: 0.1582 | R2: 0.6152
+Epoch 135/150 | Loss: 0.1562 | R2: 0.6172
+Epoch 140/150 | Loss: 0.1544 | R2: 0.6154
+Epoch 145/150 | Loss: 0.1534 | R2: 0.6133
+Epoch 150/150 | Loss: 0.1524 | R2: 0.6182
+TANR3 Time: 13.92s | Final R2: 0.6182
+Baseline Latent Space PCA explained variance: 0.7332
+TANR3 Latent Space PCA explained variance: 0.9137
 
 Final Results:
-baseline: 0.5530
-TANR64: 0.6110
-TANR8: 0.5205
-TANR3: 0.4934
+baseline: 0.5945
+TANR64: 0.5239
+TANR8: 0.5690
+TANR3: 0.6182
