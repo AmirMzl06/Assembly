691aad0f063718f5954d573dabe14e3fdb7c0349
AmirMzl06 <134225485+AmirMzl06@users.noreply.github.com>
2026-08-16 09:40:54 -0400

    Update First.s

--- First.s
+++ First.s
@@ -1,32 +1,18 @@
-mirzaei@haas034:~$ mkdir ~/robust-cebra-v5
-cd ~/robust-cebra-v5
-mirzaei@haas034:~/robust-cebra-v5$ nano Dockerfile
-mirzaei@haas034:~/robust-cebra-v5$ podman build \
-WARN[0000] Network file system detected as backing store.  Enforcing overlay option `force_mask="700"`.  Add it to storage.conf to silence this warning 
-Error: no context directory and no Containerfile specified
-mirzaei@haas034:~/robust-cebra-v5$ -t registry.rcp.epfl.ch/upmwmathis-mirzaei/robust-cebra:v0.5 .
--t: command not found
-mirzaei@haas034:~/robust-cebra-v5$ podman build \
+mirzaei@haas034:~/robust-cebra-v5$ podman --storage-driver=vfs \
+--root /tmp/$USER/podman-storage \
+build \
 -t registry.rcp.epfl.ch/upmwmathis-mirzaei/robust-cebra:v0.5 .
-WARN[0000] Network file system detected as backing store.  Enforcing overlay option `force_mask="700"`.  Add it to storage.conf to silence this warning 
 STEP 1/4: FROM registry.rcp.epfl.ch/upmwmathis-mirzaei/robust-cebra:v0.4
-Trying to pull registry.rcp.epfl.ch/upmwmathis-mirzaei/robust-cebra:v0.4...
-Getting image source signatures
-Copying blob ec6d5f6c9ed9 done   | 
-Copying blob 7021d1b70935 done   | 
-Copying blob 0a7674e3e8fe done   | 
-Copying blob 0d6448aff889 done   | 
-Copying blob 56dc85502937 done   | 
-Copying blob b71b637b97c5 done   | 
-Copying blob 47b8539d532f done   | 
-Copying blob fd9cc1ad8dee done   | 
-Copying blob 83525caeeb35 done   | 
-Copying blob 59cca8ee4425 done   | 
-Copying blob 2d02ca966369 done   | 
-Copying blob eaa289c4d0c1 done   | 
-Error: creating build container: writing blob: adding layer with blob "sha256:7021d1b70935851c95c45ed18156980b5024eda29b99564429025ea04f5ec109": processing tar file(lsetxattr /boot: operation not supported): exit status 1
-mirzaei@haas034:~/robust-cebra-v5$ podman push \
-registry.rcp.epfl.ch/upmwmathis-mirzaei/robust-cebra:v0.5
-WARN[0000] Network file system detected as backing store.  Enforcing overlay option `force_mask="700"`.  Add it to storage.conf to silence this warning 
-Error: registry.rcp.epfl.ch/upmwmathis-mirzaei/robust-cebra:v0.5: image not known
+STEP 2/4: USER root
+--> Using cache a19712ec72ef00ee16debabf55f1d56ca8f9cafb238e2f52992b1783c9db8989
+--> a19712ec72ef
+STEP 3/4: RUN apt-get update &&     apt-get install -y gcc g++ make &&     rm -rf /var/lib/apt/lists/*
+--> Using cache 7c4284e17eb98ad6cf88edd95764e5d1230c4f4c031d9dc67d029dad657a7ce2
+--> 7c4284e17eb9
+STEP 4/4: USER 270850
+--> Using cache a057214f0e93501dac9b46ebd7a82b4fb755f3c1ca9298c0374689efa4766631
+COMMIT registry.rcp.epfl.ch/upmwmathis-mirzaei/robust-cebra:v0.5
+--> a057214f0e93
+Successfully tagged registry.rcp.epfl.ch/upmwmathis-mirzaei/robust-cebra:v0.5
+a057214f0e93501dac9b46ebd7a82b4fb755f3c1ca9298c0374689efa4766631
 mirzaei@haas034:~/robust-cebra-v5$ 
