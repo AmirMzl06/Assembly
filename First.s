00b5d74e96f19abcaf9ff38c24d8f45441de0c0c
AmirMzl06 <134225485+AmirMzl06@users.noreply.github.com>
2026-08-16 09:34:16 -0400

    Update First.s

--- First.s
+++ First.s
@@ -1,6 +1,12 @@
-mirzaei@haas034:~$ docker --version
-Command 'docker' not found, but can be installed with:
-apt install docker.io      # version 29.1.3-0ubuntu3~24.04.2, or
-apt install podman-docker  # version 4.9.3+ds1-1ubuntu0.2
-Ask your administrator to install one of them.
+mirzaei@haas034:~$ which apptainer
+which singularity
+which buildah
+which podman
+which kaniko
+/usr/bin/buildah
+/usr/bin/podman
+mirzaei@haas034:~$ ls /usr/bin | grep -E "apptainer|singularity|buildah|podman"
+buildah
+podman
+podmansh
 mirzaei@haas034:~$ 
