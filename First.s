2dfcfde0e4451c2a02fe6457dfb2692d1996b404
AmirMzl06 <134225485+AmirMzl06@users.noreply.github.com>
2026-08-16 09:55:48 -0400

    Update First.s

--- First.s
+++ First.s
@@ -1,29 +1,6 @@
-I have no name!@mm8-v5-gcc-0-0:~/sam/result/Aggregate$ exit
-exit
-mirzaei@haas034:~/sam/result/Aggregate$ cd
-mirzaei@haas034:~$ podman --storage-driver=vfs \
---root /tmp/$USER/podman-storage \
-push \
-registry.rcp.epfl.ch/upmwmathis-mirzaei/robust-cebra:v0.5
-Getting image source signatures
-Copying blob ec6d5f6c9ed9 skipped: already exists  
-Copying blob 7021d1b70935 skipped: already exists  
-Copying blob 0d6448aff889 skipped: already exists  
-Copying blob 0a7674e3e8fe skipped: already exists  
-Copying blob b71b637b97c5 skipped: already exists  
-Copying blob 56dc85502937 skipped: already exists  
-Copying blob 47b8539d532f skipped: already exists  
-Copying blob 124c0f2e3c9f [----------------------------------] 8.0b / 225.1MiB | 1.3 MiB/s
-Copying blob fd9cc1ad8dee skipped: already exists  
-Copying blob 59cca8ee4425 skipped: already exists  
-Copying blob 2d02ca966369 skipped: already exists  
-Copying blob 83525caeeb35 skipped: already exists  
-Copying blob eaa289c4d0c1 skipped: already exists  
-Error: writing blob: initiating layer upload to /v2/upmwmathis-mirzaei/robust-cebra/blobs/uploads/ in registry.rcp.epfl.ch: unauthorized: unauthorized to access repository: upmwmathis-mirzaei/robust-cebra, action: push: unauthorized to access repository: upmwmathis-mirzaei/robust-cebra, action: push
-mirzaei@haas034:~$ runai delete job mm8-v5-gcc -p upmwmathis-mirzaei
-Job mm8-v5-gcc deleted successfully.
-mirzaei@haas034:~$ runai submit --name mm8-v6-gcc \
-  --image registry.rcp.epfl.ch/upmwmathis-mirzaei/robust-cebra:v0.5 \
+mirzaei@haas034:~$ 
+runai submit --name mm8 \
+  --image registry.rcp.epfl.ch/upmwmathis-mirzaei/robust-cebra:v0.4 \
   --gpu 1 --cpu 64 --memory 256Gi --node-pools h100 --large-shm \
   --pvc home:/home/mirzaei \
   --pvc upmwmathis-scratch:/data \
@@ -31,7 +8,32 @@ mirzaei@haas034:~$ runai submit --name mm8-v6-gcc \
   -p upmwmathis-mirzaei \
   --attach --run-as-user \
   --command -- bash -lc 'bash'
-Job mm8-v6-gcc submitted successfully.
+Job mm8 submitted successfully.
 You can check the status of the job by running:
-	runai describe job mm8-v6-gcc -p upmwmathis-mirzaei
-
+	runai describe job mm8 -p upmwmathis-mirzaei
+Connecting to pod mm8-0-0
+If you don't see a command prompt, try pressing enter.
+I have no name!@mm8-0-0:/workspace$ cd /home/mirzaei/sam/result/Aggregate
+I have no name!@mm8-0-0:~/sam/result/Aggregate$ python -m pip show torch
+python -m pip show triton
+Name: torch
+Version: 2.13.0
+Summary: Tensors and Dynamic neural networks in Python with strong GPU acceleration
+Home-page: https://pytorch.org
+Author: 
+Author-email: PyTorch Team <packages@pytorch.org>
+License-Expression: Apache-2.0 AND Apache-2.0 WITH LLVM-exception AND BSD-2-Clause AND BSD-3-Clause AND BSL-1.0 AND MIT
+Location: /home/mirzaei/.local/lib/python3.10/site-packages
+Requires: cuda-bindings, cuda-toolkit, filelock, fsspec, jinja2, networkx, nvidia-cudnn-cu13, nvidia-cusparselt-cu13, nvidia-nccl-cu13, nvidia-nvshmem-cu13, setuptools, sympy, triton, typing-extensions
+Required-by: accelerate, captum, neural_decoder, torchtyping
+Name: triton
+Version: 3.7.1
+Summary: A language and compiler for custom Deep Learning operations
+Home-page: https://github.com/triton-lang/triton/
+Author: Philippe Tillet
+Author-email: phil@openai.com
+License: 
+Location: /home/mirzaei/.local/lib/python3.10/site-packages
+Requires: 
+Required-by: torch
+I have no name!@mm8-0-0:~/sam/result/Aggregate$ 
