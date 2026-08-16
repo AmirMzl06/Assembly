7bd57f4739a164ac431bc22f9c5e719c4fae44a1
AmirMzl06 <134225485+AmirMzl06@users.noreply.github.com>
2026-08-16 10:20:41 -0400

    Update First.s

--- First.s
+++ First.s
@@ -1,12 +1,18 @@
-(.venv) I have no name!@mm8-0-0:~/sam/result/Aggregate$ echo $CUDA_HOME
-echo $LD_LIBRARY_PATH
-
-/usr/local/nvidia/lib:/usr/local/nvidia/lib64
-(.venv) I have no name!@mm8-0-0:~/sam/result/Aggregate$ find / -name "libnvrtc-builtins.so*" 2>/dev/null
-/home/mirzaei/.local/lib/python3.10/site-packages/nvidia/cuda_nvrtc/lib/libnvrtc-builtins.so.12.8
-/home/mirzaei/.local/lib/python3.10/site-packages/nvidia/cuda_nvrtc/lib/libnvrtc-builtins.so.11.7
-/home/mirzaei/.local/lib/python3.10/site-packages/nvidia/cu13/lib/libnvrtc-builtins.so.13.0
-/home/mirzaei/lm-cebra/.venv/lib/python3.10/site-packages/nvidia/cuda_nvrtc/lib/libnvrtc-builtins.so.12.4
-/home/mirzaei/lm-cebra/.venv/lib/python3.10/site-packages/nvidia/cu13/lib/libnvrtc-builtins.so.13.0
-/home/mirzaei/aj_project/cat/.venv/lib/python3.10/site-packages/nvidia/cuda_nvrtc/lib/libnvrtc-builtins.so.12.4
-/home/mirzaei/.cebra_venv/lib/python3.10/site-packages/nvidia/cuda_nvrtc/lib/libnvrtc-builtins.so.12.4
+Last login: Sun Aug 16 15:53:07 2026 from 128.179.253.32
+groups: cannot find name for group ID 30299
+mirzaei@haas034:~$ runai delete job mm8 -p upmwmathis-mirzaei
+Job mm8 deleted successfully.
+mirzaei@haas034:~$ runai submit --name mm8 \
+  --image registry.rcp.epfl.ch/upmwmathis-mirzaei/robust-cebra:v0.4 \
+  --gpu 1 --cpu 64 --memory 256Gi --node-pools h100 --large-shm \
+  --pvc home:/home/mirzaei \
+  --pvc upmwmathis-scratch:/data \
+  -e HOME=/home/mirzaei \
+  -p upmwmathis-mirzaei \
+  --attach --run-as-user \
+  --command -- bash -lc 'bash'
+Job mm8 submitted successfully.
+You can check the status of the job by running:
+	runai describe job mm8 -p upmwmathis-mirzaei
+could not find any job with the given name mm8
+mirzaei@haas034:~$ 
