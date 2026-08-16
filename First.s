6ffdccae7f8036c2d4a85089a7030c991b5acc34
AmirMzl06 <134225485+AmirMzl06@users.noreply.github.com>
2026-08-16 10:04:51 -0400

    Update First.s

--- First.s
+++ First.s
@@ -1,16 +1,12 @@
-I have no name!@mm8-0-0:~/sam/result/Aggregate$ mv ~/.local/lib/python3.10/site-packages/torch ~/.local/lib/python3.10/site-packages/torch_backup
-mv ~/.local/lib/python3.10/site-packages/triton ~/.local/lib/python3.10/site-packages/triton_backup
-I have no name!@mm8-0-0:~/sam/result/Aggregate$ python -c "import torch; print(torch.__file__, torch.__version__)"
-Traceback (most recent call last):
-  File "<string>", line 1, in <module>
-ModuleNotFoundError: No module named 'torch'
-I have no name!@mm8-0-0:~/sam/result/Aggregate$ python -m pip list | grep -E "torch|triton"
-torch                        2.13.0
-torchtyping                  0.1.5
-triton                       3.7.1
-I have no name!@mm8-0-0:~/sam/result/Aggregate$ python -c "import torch; print(torch.__file__)"
-Traceback (most recent call last):
-  File "<string>", line 1, in <module>
-ModuleNotFoundError: No module named 'torch'
+I have no name!@mm8-0-0:~/sam/result/Aggregate$ ls ~/.local/lib/python3.10/site-packages | grep torch
+functorch
+torch-2.13.0.dist-info
+torch_backup
+torchgen
+torchtyping
+torchtyping-0.1.5.dist-info
+I have no name!@mm8-0-0:~/sam/result/Aggregate$ ls /usr/local/lib/python3.10/site-packages | grep torch
+ls: cannot access '/usr/local/lib/python3.10/site-packages': No such file or directory
+I have no name!@mm8-0-0:~/sam/result/Aggregate$ python -c "import sys; print(sys.path)"
+['', '/usr/lib/python310.zip', '/usr/lib/python3.10', '/usr/lib/python3.10/lib-dynload', '/home/mirzaei/.local/lib/python3.10/site-packages', '/usr/local/lib/python3.10/dist-packages', '/usr/lib/python3/dist-packages']
 I have no name!@mm8-0-0:~/sam/result/Aggregate$ 
-
